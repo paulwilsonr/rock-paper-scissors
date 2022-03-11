1dab8e0c34a5b0dac78b2d5a581958b147e54b30
Paul Wilson <paul.wilson.r@gmail.com>
2022-03-11 10:45:09 -0600

    Rework the game using querySelectorAll

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let computerPlay = () => {
     }
     return choice;
 }
-
+/*
 let playGame = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
     switch(playerSelection) {
@@ -47,23 +47,53 @@ let playGame = (playerSelection, computerSelection) => {
             }
     }
 }
+*/
 
+const playGame = (playerChoice, computerSelection) => {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerSelection) {
+        return {message:`It's a Tie! You both picked ${playerChoice}`, results: 'tie'};
+    } else {
+        switch(playerChoice) {
+            case 'rock':
+                if (computerSelection === 'paper') {
+                    return {message:'You Lose! Paper beats Rock.', results: 'loss'};
+                } else if (computerSelection === 'scissors') {
+                    return {message:'You Win! Rock beats Scissors.', results: 'win'};
+        }
+        case 'paper':
+            if (computerSelection === 'scissors') {
+                return {message:'You Lose! Scissors beats Paper.', results: 'loss'};
+            } else if (computerSelection === 'rock') {
+                return {message:'You Win! Paper beats Rock.', results: 'win'};
+            }
+            case 'scissors':
+                if (computerSelection === 'rock') {
+                    return {message:'You Lose! Rock beats Scissors.', results: 'loss'};
+                } else if (computerSelection === 'paper') {
+                    return {message:'You Win! Scissors beats Paper.', results: 'win'};
+                }
+    }
+}
+};
 function changeScore(newWin, newLose) {
     document.getElementById("win").innerHTML = `Wins: ${newWin}`;
     document.getElementById("lose").innerHTML = `Losses: ${newLose}`;
 }
 
-let game = (playerChoice) => {
-        let winMessage = playGame(playerChoice, computerPlay());
-        if(winMessage[4] === 'L') {
+
+let game = (e) => {
+        let playerChoice = e.target.innerText;
+         let winMessage = playGame(playerChoice, computerPlay());
+        if(winMessage.results === 'loss') {
             losses++;
             changeScore(wins, losses);
-        } else if (winMessage[4] === 'W') {
+        } else if (winMessage.results === 'win') {
             wins++;
             changeScore(wins, losses);
         }
-        document.getElementById("win-message").innerHTML = winMessage;
-        document.getElementById("win-message").style.visibility = "visible";
+        document.getElementById("win-message").textContent = winMessage.message;
+        document.getElementById("win-message").style.visibility = "visible"; 
 };
 
 function resetScore() {
@@ -72,3 +102,7 @@ function resetScore() {
     changeScore(wins, losses);
     document.getElementById("win-message").style.visibility = "hidden";
 };
+
+const playerChoices = document.querySelectorAll('.playerChoice');
+
+playerChoices.forEach(playerChoice => playerChoice.addEventListener('click', game));
